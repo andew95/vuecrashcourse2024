f09ee7e82f86ad1aa8624bd606a48357d0ebf31b
Apiwut <apiwut_t@outlook.com>
2024-12-18 23:49:31 +0700

    fix submit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import { ref } from 'vue'
 const name = ref('John Doe')
 const status = ref('active')
 const tasks = ref(['Task one', 'Task two', 'Task three'])
+const link = 'https://google.com'
+
+const newTask = ref('Task four')
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -14,6 +17,13 @@ const toggleStatus = () => {
     status.value = 'active'
   }
 }
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
 </script>
 
 <template>
@@ -22,6 +32,13 @@ const toggleStatus = () => {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Task</h3>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
